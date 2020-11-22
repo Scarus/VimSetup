725e3e55b1c45fa496e50d17f47384df35d449da
Pierre 'Scarus' Saillard <pierresaillard1@gmail.com>
2020-11-22 21:00:03 +0100

    automatize vim-commentary plugin package installation

--- setup_vim.sh
+++ setup_vim.sh
@@ -5,18 +5,24 @@ vim-nerdtree
 vim-surround
 vim-tagbar
 vim-syntastic
-vim-nerdcommenter
 vim-latexsuite
 vim-molokai
 vim-seti
 vim-ale
 "
 
+sudo pacman -Sy vim
+sudo pacman -Sy $PACKAGES
+
+# install vim-omnisharp
 VIMPLUGINPATH="$HOME/.vim/pack/plugins/start"
 mkdir -p $VIMPLUGINPATH
 cd $VIMPLUGINPATH
 git clone git://github.com/OmniSharp/omnisharp-vim
 
-cd -
-sudo pacman -Sy vim
-sudo pacman -Sy $PACKAGES
+# install vim-commentary (instead of vim-nerdcommenter to match with vim for vscode)
+VIMPLUGINPATH="$HOME/.vim/pack/tpope/start"
+mkdir -p $VIMPLUGINPATH
+cd $VIMPLUGINPATH
+git clone https://tpope.io/vim/commentary.git
+vim -u NONE -c "helptags commentary/doc" -c q
